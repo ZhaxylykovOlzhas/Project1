e2cc725f9b74ecaf56ec07b8258d4143f66deab6
zhaxylykov <zhaxylykov_ob@mail.ru>
2018-09-12 17:41:56 +0600

    first commit

---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //1)
+           // string Name;
+           // string LastName;
+
+           // Console.WriteLine("Введите ваше имя:");
+
+           // Name = Convert.ToString(Console.ReadLine());
+           // Console.WriteLine("Введите ваше Фамилия:");
+           // LastName = Convert.ToString(Console.ReadLine());
+           // Console.WriteLine("Вы: " + Name + " " + LastName);
+           // Console.ReadLine();
+
+
+
+           // 2)
+
+           // int numbers1;
+           // int numbers2;
+           // int numbers3;
+
+           // Console.WriteLine("Введите 1- число:");
+           // numbers1 = Convert.ToInt32(Console.ReadLine());
+
+           // Console.WriteLine("Введите 2- число:");
+           // numbers2 = Convert.ToInt32(Console.ReadLine());
+
+           // Console.WriteLine("Введите 3- число:");
+           // numbers3 = Convert.ToInt32(Console.ReadLine());
+
+           // int summa = 0;
+           // summa += numbers1 + numbers2 + numbers3;
+           // Console.WriteLine("Общая сумма:" + summa);
+
+           // Console.ReadLine();
+
+
+        }
+    }
+}
